1c2710e8db80facdb3100252843426be4f7d14bb
hstrodtman <heatherstrodtman@gmail.com>
2020-01-10 10:51:08 -0600

    Updated the printJobs method.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,15 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("There were no jobs to print.");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("******");
+                for (HashMap.Entry<String, String> item : job.entrySet()){
+                    System.out.println(item.getKey() +":" + " (" + item.getValue() + ")");
+                }
+            }
+        }
     }
 }
